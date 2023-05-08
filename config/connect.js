7b918e56c2af29d351d0b68d8461309403e9604a
nabil_khmissi <bilakhmissi@gmail.com>
2023-05-08 21:50:07 +0100

    search by hero name

--- config/connect.js
+++ config/connect.js
@@ -1,8 +1,7 @@
 // 1
 const mongoose = require('mongoose');
 // 2
-// mongoose.connect( 'mongodb://127.0.0.1:27017/hero' )
-mongoose.connect( 'mongodb+srv://system:system@cluster0.8dld4l3.mongodb.net/hero?retryWrites=true&w=majority' )
+mongoose.connect( 'mongodb://127.0.0.1:27017/hero' )
     .then(
         ()=>{
             console.log('connected to db !');
